e8c3ead59b17328058448eb41ef174a5dbaaaf8c
amanashutosh <aman.ashutosh357@gmail.com>
2025-07-01 17:26:15 +0530

    moved to local machine

--- JSFOUNDATION/08_projects/1_colorChanger/script.js
+++ JSFOUNDATION/08_projects/1_colorChanger/script.js
@@ -1,18 +1,9 @@
-# Projects related to DOM
-
-# Solution code
-
-## project 1
-
-```javascript
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 
 buttons.forEach(function (button) {
   console.log(button);
   button.addEventListener("click", function (e) {
-    console.log(e);
-    console.log(e.target);
     if (e.target.id === "grey") {
       body.style.backgroundColor = e.target.id;
     }
@@ -27,10 +18,3 @@ buttons.forEach(function (button) {
     }
   });
 });
-```
-
-## Project 2
-
-```
-
-```
