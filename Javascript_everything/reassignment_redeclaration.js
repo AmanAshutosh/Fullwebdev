1686ed95a71780357bc3529bae60cf1b931dda5a
amanashutosh <aman.ashutosh357@gmail.com>
2025-07-29 20:14:49 +0530

    reassign nd redeckaration

---
+++ Javascript_everything/reassignment_redeclaration.js
@@ -0,0 +1,10 @@
+// Reassignment, redeclaration
+
+/*
+Reassignment vs Redeclaration
+
+Keyword      	Reassignable?	   Redeclarable in same scope?
+var               yes                yes
+let               yes                No
+const             No                 No
+*/
