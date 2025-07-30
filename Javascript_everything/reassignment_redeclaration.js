5de8763a50ebfc7a127c6957ecbcb40d6073eb49
amanashutosh <aman.ashutosh357@gmail.com>
2025-07-30 19:51:23 +0530

    updateed

--- Javascript_everything/reassignment_redeclaration.js
+++ Javascript_everything/reassignment_redeclaration.js
@@ -8,3 +8,16 @@ var               yes                yes
 let               yes                No
 const             No                 No
 */
+
+// snippet 1
+
+var a = 12;
+a = 32;
+
+var a = 20; //  allowed  
+
+
+let b = 12;
+b = 32;
+
+// let b = 23; //Error
